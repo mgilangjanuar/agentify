d70160bbce382e7fdac66a2b1962a3f60507d52b
M Gilang Januar <mgilangjanuar@gmail.com>
2024-07-08 19:03:12 +0700

    chore: Update MyAgents page with grid layout and increased spacing
    The code changes update the MyAgents page by replacing the flex layout with a grid layout and increasing the spacing between elements. This change improves the visual layout and enhances the readability of the page.

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -7,13 +7,16 @@ import { cn } from '@/lib/utils'
 export default function Home() {
   return <div>
     <div className="relative flex h-[60svh] w-full flex-col items-center justify-center overflow-hidden rounded-lg bg-background container mx-auto max-w-3xl text-center">
-      <div className="space-y-4 z-20">
+      <div className="space-y-3 z-20">
         <Badge>
           Introducing Agent Studio ✨
         </Badge>
         <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-6xl">
-          Build &amp; Install LLM Agents with <span className="underline underline-offset-4">No Code</span>
+          Build &amp; Install LLM Agents with <span className="underline underline-offset-8">No Code</span>
         </h1>
+        <p className="leading-7 !mt-6 text-muted-foreground lg:text-xl">
+          Create, publish, and install AI agents. Powered by <strong>Claude 3.5 Sonnet</strong> model.
+        </p>
       </div>
       <DotPattern
         className={cn(
