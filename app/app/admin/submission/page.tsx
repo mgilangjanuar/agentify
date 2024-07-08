436dfa299a3d06548237faa9609f53cba8810447
M Gilang Januar <mgilangjanuar@gmail.com>
2024-07-08 14:12:00 +0700

    chore: Update MyAgents page with grid layout and increased spacing
    The code changes update the MyAgents page by replacing the flex layout with a grid layout and increasing the spacing between elements. This change improves the visual layout and enhances the readability of the page.

--- app/app/admin/submission/page.tsx
+++ app/app/admin/submission/page.tsx
@@ -2,6 +2,7 @@
 
 import { Button } from '@/components/ui/button'
 import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from '@/components/ui/sheet'
 import { useUser } from '@/components/use-user'
 import { hit } from '@/lib/hit'
 import { Agent } from '@prisma/client'
@@ -41,17 +42,34 @@ export default function Submission() {
     <div className="grid gap-2 xl:grid-cols-3 sm:grid-cols-2 grid-cols-1">
       {(agents || [])?.map(agent => (
         <Card key={agent.id}>
-          <CardHeader>
-            <div className="flex gap-4 flex-nowrap">
-              {agent.logoUrl ? <Image src={agent.logoUrl} width={50} height={50} className="rounded-lg !size-12" alt={agent.name} /> : <div className="!w-12 !h-12 flex items-center justify-center rounded-lg bg-gray-200">
-                <LucideBot className="h-6 w-6" />
-              </div>}
-              <div className="flex flex-col space-y-1.5 flex-1">
-                <CardTitle>{agent.name}</CardTitle>
-                <CardDescription className="line-clamp-2 min-h-[40px]">{agent.description}</CardDescription>
+          <Sheet>
+            <SheetTrigger asChild>
+              <CardHeader className="hover:cursor-pointer">
+                <div className="flex gap-4 flex-nowrap">
+                  {agent.logoUrl ? <Image src={agent.logoUrl} width={50} height={50} className="rounded-lg !size-12" alt={agent.name} /> : <div className="!w-12 !h-12 flex items-center justify-center rounded-lg bg-gray-200">
+                    <LucideBot className="h-6 w-6" />
+                  </div>}
+                  <div className="flex flex-col space-y-1.5 flex-1">
+                    <CardTitle>{agent.name}</CardTitle>
+                    <CardDescription className="line-clamp-2 min-h-[40px]">{agent.description}</CardDescription>
+                  </div>
+                </div>
+              </CardHeader>
+            </SheetTrigger>
+            <SheetContent>
+              <SheetHeader className="!text-left">
+                <SheetTitle>{agent.name}</SheetTitle>
+                <SheetDescription className="line-clamp-2">
+                  {agent.description}
+                </SheetDescription>
+              </SheetHeader>
+              <div className="py-4">
+                <pre className="w-full text-sm overflow-auto no-scrollbar font-mono p-4 bg-muted rounded-lg max-h-[calc(100svh-150px)]">
+                  {JSON.stringify(agent, null, 2)}
+                </pre>
               </div>
-            </div>
-          </CardHeader>
+            </SheetContent>
+          </Sheet>
           <CardFooter className="flex gap-3 items-center justify-between flex-nowrap">
             <p className="text-muted-foreground truncate text-xs">
               By <span className="font-medium">{agent.user.name}</span>
