61127f4286df90c57467377248aaaba28966f670
M Gilang Januar <mgilangjanuar@gmail.com>
2024-07-09 16:16:56 +0700

    chore: Update Markdown component heading styles
    The code changes in this commit update the heading styles in the Markdown component. The `mt-6` class has been added to all heading elements, providing consistent spacing between the headings and the content. This change improves the visual hierarchy and readability of the rendered Markdown content.

--- components/markdown.tsx
+++ components/markdown.tsx
@@ -25,22 +25,22 @@ export default function Markdown({ content }: { content: string }) {
         )
       },
       h1({ children }) {
-        return <h1 className="scroll-m-20 text-4xl pb-4 font-extrabold tracking-tight">
+        return <h1 className="mt-6 scroll-m-20 text-4xl pb-4 font-extrabold tracking-tight">
           {children}
         </h1>
       },
       h2({ children }) {
-        return <h2 className="scroll-m-20 border-b- pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+        return <h2 className="mt-6 scroll-m-20 border-b- pb-2 text-3xl font-semibold tracking-tight first:mt-0">
           {children}
         </h2>
       },
       h3({ children }) {
-        return <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+        return <h3 className="mt-6 scroll-m-20 text-2xl font-semibold tracking-tight">
           {children}
         </h3>
       },
       h4({ children }) {
-        return <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+        return <h4 className="mt-6 scroll-m-20 text-xl font-semibold tracking-tight">
           {children}
         </h4>
       },
