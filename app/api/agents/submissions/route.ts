9022d9b6b8db1271f6cd8a9d1187683776db08fe
M Gilang Januar <mgilangjanuar@gmail.com>
2024-07-09 22:59:16 +0700

    refactor: Update ClaudeReceiver class to split lines using '\r\n\r\n'

--- app/api/agents/submissions/route.ts
+++ app/api/agents/submissions/route.ts
@@ -15,6 +15,9 @@ export const GET = authorization(async (req) => {
     where: {
       isPublic: true
     },
+    orderBy: {
+      name: 'asc'
+    },
     include: {
       user: {
         select: {
